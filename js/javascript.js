ac07ff19bdbc5f59577a2e6fcdd69b84a7f4629b
e <314339793@qq.com>
2016-03-13 16:52:00 +0800

    add js
    一些效果的实现

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,48 @@
-var plane = document.getElementById("plane");
+window.onload = function(){
+	var flying = setInterval("fly()",time);//调用fly();
+	var funny = function(i){//判断点击数组的序号，显示相应的介绍信息
+        buttons[i].onclick = function(){
+        	showInformation(i);
+            }
+    }
+	for(var i=0;i<buttons.length;i++){
+        funny(i);
+    }
+}
+
+//fly
+var planes = document.getElementById("plane");
 var flyWidth = document.body.clientWidth;
+var time = 10;
 function fly(){
-	if (plane.style.left>0) {
-		plane.style.left--;
+	if (planes.offsetLeft<-234) {
+			planes.style.left = flyWidth+100+"px";
 	}else{
-		plane.style.left = flyWidth;
+		planes.style.left = planes.offsetLeft -1 +"px";
 	}
 }
-var flying = setInterval("")
+function getElementsByClassName(tagName,className) {  
+    var tag = document.getElementsByTagName(tagName);  
+    var tagAll = [];  
+    for(var i = 0 ; i<tag.length ; i++){  
+        if(tag[i].className.indexOf(className) != -1){  
+            tagAll[tagAll.length] = tag[i];  
+        }  
+    }  
+    return tagAll;     
+}
+//information 	
+var buttons = getElementsByClassName("div","avatar");
+var textareas = getElementsByClassName("div","textarea");
+function showInformation(i){
+	if(textareas[i].style.display == "block"){
+		textareas[i].style.display = "none";
+	}
+	if(textareas[i].style.display == "none"){
+		for(var j=0;j<textareas.length;j++){
+			textareas[j].style.display = "none";
+		}
+		textareas[i].style.display = "block";
+	}
+	
+}
